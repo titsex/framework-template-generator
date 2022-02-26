078eb238561f6116031508572e788cb60f1edb55
titsex <61911688+titsex@users.noreply.github.com>
2022-02-26 14:33:08 +0300

    Revert "⚡Almost ready"
    This reverts commit c5c15ef8c566e45ad0968c6e4b9db67bed453869.

--- build/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.render = void 0;
-const tslib_1 = require("tslib");
-const ejs = (0, tslib_1.__importStar)(require("ejs"));
-function render(content, data) {
-    return ejs.render(content, data);
-}
-exports.render = render;
